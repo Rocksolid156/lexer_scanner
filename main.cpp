a3b094ce8e79477c54df0b3793e8993d3c0bcdbc
kanom <lhaotian953@gmail.com>
2024-05-12 10:42:37 +0800

    实现除了自动机功能以外的大部分功能，现在无法判断标识符，常量

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ const int num_of_Boperator = 20;  //双目运算符
 const char Keyword[num_of_keyword][12] = {"include", "int", "char", "if", "else", "void", "while", "return", "float",
                                           "double", "break", "case", "switch", "default", "continue", "long", "typedef",
                                           "iostream", "do", "define", "for", "const", "struct"};    //关键字
-const char Delimiter[num_of_delimiter] = {',', '(', ')', '{', '}', ';', '<', '>', '#'};   //界符
+const char Delimiter[num_of_delimiter] = {',', '(', ')', '{', '}', ';', '<', '>', '#', '\'', '\"'};   //界符
 const char Monocular_Operator[num_of_Moperator] = {'+', '-', '*', '/', '%', '!', '<', '>', '=', '~', '&', '|',
                                                    '=', '^'};    //单目运算符
 const char Binocular_Operator[num_of_Boperator][5] = {"<=", ">=", "==", "!=", "&&", "||", "+=", "-=", "*=", "/=", "++",
@@ -25,6 +25,8 @@ const char Binocular_Operator[num_of_Boperator][5] = {"<=", ">=", "==", "!=", "&
 ifstream input;
 ofstream output;
 FILE *file_source = nullptr; //文件指针默认值
+
+/*Top of //TODO */
 
 struct NFA_set{
     char set[100];
@@ -35,6 +37,8 @@ NFA_set Move[100][100];
 int dfa[100][100];  //DFA状态转换函数实现：δ(当前状态，输入字母)
 bool isFinal[100]; //某状态是否为终态，例如isFinal[1]
 int len_state,len_final;
+
+/*Bottom of //TODO */
 /***************************************
  * include -> l 0
  * int -> I 1
@@ -173,6 +177,18 @@ bool DFA(char str[]) {   //自动机
     return false;
 }
 
+void init() {
+    len_final = 0;
+    len_state = 0;
+    for (int i = 0; i < 100; ++i) {
+        for (int j = 0; j < 100; ++j) {
+            for (int k = 0; k < 100; ++k) {
+                Move[i][j].set[k] = '#';
+            }
+        }
+    }
+}
+
 void scan() {
     char str[100];
     char ch;
@@ -182,11 +198,35 @@ void scan() {
     ch = fgetc(file_source);
     /*读取文件,每成功读取一个字符，指针自动跳到下一个字符
      * 先调用函数给if，来判断开头的字母是什么，然后判断下一步走向
+     * fgetc将被看待为书中的GetNextChar()函数
      */
     bool isFinish = false;   //default
     while (!isFinish) {
         flag = -1;    //default
-        point = 0;
+        point = 0;  //default指针
+        if (isLetter(ch)) {
+            flag = 2;
+            str[point++] = ch;
+            ch = fgetc(file_source);
+            while (isLetter(ch) || isInteger(ch) || ch == '\\') {
+                flag = 2;
+                str[point++] = ch;
+                ch = fgetc(file_source);
+            }
+            str[point] = '\0';
+        }   //P49（3）的实现
+        if (flag == 2) {
+            if (isKeyword(str)) {
+                cout << str << " " << "关键字" << endl;
+                output << GetToken(str);
+            } else {
+                if (DFA(str)) { //TODO
+                    cout << str << " " << "标识符" << endl;
+                    output << 2;
+                } else
+                    cout << str << " " << "不是标识符，错误" << endl;
+            }
+        }
         if (isInteger(ch)) { //判断当前单词是否为整数，若不是就判断为其他种类单词
             flag = 1;
             str[point++] = ch;    //先赋值再自加
@@ -200,38 +240,15 @@ void scan() {
                 ch = fgetc(file_source);
             }
             str[point] = '\0';    //字符串结尾，分割出一个已经判断出的词语
-        }
+        }   //P49（4）的实现
         if (flag == 1) {
-            if (DFA(str)) {
+            if (DFA(str)) { //TODO
                 cout << str << " " << "为常量" << endl;
                 output << 3;
             } else
                 cout << str << " " << "不是常量，错误" << endl;
             point = 0;    //重置指针
             flag = -1;    //重置浮标
-        }
-        if (isLetter(ch)) {
-            flag = 2;
-            str[point++] = ch;
-            ch = fgetc(file_source);
-            while (isLetter(ch) || isInteger(ch) || ch == '\\') {
-                flag = 2;
-                str[point++] = ch;
-                ch = fgetc(file_source);
-            }
-            str[point] = '\0';
-        }
-        if (flag == 2) {
-            if (isKeyword(str)) {
-                cout << str << " " << "关键字" << endl;
-                output << GetToken(str);
-            } else {
-                if (DFA(str)) {
-                    cout << str << " " << "标识符" << endl;
-                    output << 2;
-                } else
-                    cout << str << " " << "不是标识符，错误" << endl;
-            }
         }
         if (isDelimiter(ch)) {
             cout << ch << " " << "界符" << endl;
@@ -243,6 +260,7 @@ void scan() {
                 isFinish= true;
             }
         }
+        if (flag == 3) {}  //TODO
         if(isMO(ch)){
             str[point++]=ch;
             if((ch= fgetc(file_source))==EOF){
@@ -259,27 +277,15 @@ void scan() {
                 output<<str[0];
             }
             point=0;
-        }
+        }   //P49(8)的实现
         if (ch==' '||ch=='\n'||ch=='\t'){
             if ((ch= fgetc(file_source))==EOF){
                 isFinish= true;
             }
             continue;
-        }
+        }   //P48（1）的实现
     }
     output<<'#';
-}
-
-void init(){
-    len_final=0;
-    len_state=0;
-    for (int i = 0; i < 100; ++i) {
-        for (int j = 0; j < 100; ++j) {
-            for (int k = 0; k < 100; ++k) {
-                Move[i][j].set[k]='#';
-            }
-        }
-    }
 }
 
 int main() {
